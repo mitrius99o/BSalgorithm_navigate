c265ee33ba5aa36617d3523b5a90eb1c8030b935
mitrius99 <mitrius99o@gmail.com>
2020-10-17 12:35:35 +0300

    Created winform-view(map with control parts) of the app

---
+++ DirectionsAB/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace DirectionsAB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DirectionsAB.Properties.Resources.firstfloor_land;
+            this.pictureBox1.Location = new System.Drawing.Point(4, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(811, 504);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.tabControl1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(821, 10);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(230, 500);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(3, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(228, 488);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(220, 462);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Навигатор";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(220, 462);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Об аудитории";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Откуда";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Куда";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(74, 124);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(101, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(74, 163);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(101, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 38);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Построить маршрут";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(181, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ауд.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(181, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "ауд.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 518);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
--- DirectionsAB/Program.cs
+++ DirectionsAB/Program.cs
@@ -3,128 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using static DirectionsAB.PointCommunications;
 
 namespace DirectionsAB
 {
     class Program
     {
-        /*
-        public static void CreateSapFloor(PointCommunications pc)
-        {
-            pc.CreatePoints(80);
-
-            pc.Communicate(pc.points[0], pc.points[1]);
-            pc.Communicate(pc.points[1], pc.points[2]);
-            pc.Communicate(pc.points[2], pc.points[3]);
-            pc.Communicate(pc.points[3], pc.points[4]);
-            pc.Communicate(pc.points[4], pc.points[5]);
-            pc.Communicate(pc.points[5], pc.points[6]);
-            pc.Communicate(pc.points[6], pc.points[7]);
-            pc.Communicate(pc.points[7], pc.points[8]);
-            pc.Communicate(pc.points[8], pc.points[9]);
-            pc.Communicate(pc.points[9], pc.points[10]);
-            pc.Communicate(pc.points[10], pc.points[11]);
-            pc.Communicate(pc.points[11], pc.points[12]);
-            pc.Communicate(pc.points[12], pc.points[13]);     //коридор
-            pc.Communicate(pc.points[13], pc.points[14]);
-            pc.Communicate(pc.points[14], pc.points[15]);
-            pc.Communicate(pc.points[15], pc.points[16]);
-            pc.Communicate(pc.points[16], pc.points[17]);
-            pc.Communicate(pc.points[17], pc.points[18]);
-            pc.Communicate(pc.points[18], pc.points[19]);
-            pc.Communicate(pc.points[19], pc.points[20]);
-            pc.Communicate(pc.points[20], pc.points[21]);
-            pc.Communicate(pc.points[21], pc.points[22]);
-            pc.Communicate(pc.points[22], pc.points[23]);
-            pc.Communicate(pc.points[23], pc.points[24]);
-
-
-            pc.Communicate(pc.points[0], pc.points[25]);
-            pc.Communicate(pc.points[1], pc.points[26]);
-            pc.Communicate(pc.points[2], pc.points[27]);
-            pc.Communicate(pc.points[3], pc.points[28]);
-            pc.Communicate(pc.points[4], pc.points[29]);
-            pc.Communicate(pc.points[5], pc.points[30]);
-            pc.Communicate(pc.points[6], pc.points[32]);
-            pc.Communicate(pc.points[32], pc.points[31]);
-            pc.Communicate(pc.points[7], pc.points[33]);
-            pc.Communicate(pc.points[33], pc.points[34]);
-            pc.Communicate(pc.points[8], pc.points[35]);         //аудитории северной стороны
-            pc.Communicate(pc.points[9], pc.points[36]);
-            pc.Communicate(pc.points[11], pc.points[37]);
-            pc.Communicate(pc.points[12], pc.points[40]);
-            pc.Communicate(pc.points[40], pc.points[38]);
-            pc.Communicate(pc.points[40], pc.points[41]);
-            pc.Communicate(pc.points[13], pc.points[41]);
-            pc.Communicate(pc.points[41], pc.points[39]);
-            pc.Communicate(pc.points[14], pc.points[42]);
-            pc.Communicate(pc.points[16], pc.points[43]);
-            pc.Communicate(pc.points[17], pc.points[45]);
-            pc.Communicate(pc.points[45], pc.points[44]);
-            pc.Communicate(pc.points[45], pc.points[79]);
-            pc.Communicate(pc.points[79], pc.points[46]);
-            pc.Communicate(pc.points[20], pc.points[47]);
-            pc.Communicate(pc.points[21], pc.points[48]);
-            pc.Communicate(pc.points[22], pc.points[49]);
-            pc.Communicate(pc.points[23], pc.points[50]);
-            pc.Communicate(pc.points[24], pc.points[51]);
-
-
-            pc.Communicate(pc.points[1], pc.points[52]);
-            pc.Communicate(pc.points[2], pc.points[53]);
-            pc.Communicate(pc.points[3], pc.points[54]);
-            pc.Communicate(pc.points[4], pc.points[55]);
-            pc.Communicate(pc.points[5], pc.points[56]);
-            pc.Communicate(pc.points[6], pc.points[57]);
-            pc.Communicate(pc.points[7], pc.points[59]);
-            pc.Communicate(pc.points[59], pc.points[58]);
-            pc.Communicate(pc.points[9], pc.points[60]);
-            pc.Communicate(pc.points[10], pc.points[61]);
-            pc.Communicate(pc.points[11], pc.points[62]);
-            pc.Communicate(pc.points[12], pc.points[63]);
-            pc.Communicate(pc.points[63], pc.points[64]);
-            pc.Communicate(pc.points[64], pc.points[66]);
-            pc.Communicate(pc.points[66], pc.points[65]);     //аудитории южной стороны
-            pc.Communicate(pc.points[66], pc.points[67]);
-            pc.Communicate(pc.points[67], pc.points[68]);
-            pc.Communicate(pc.points[13], pc.points[66]);
-            pc.Communicate(pc.points[15], pc.points[69]);
-            pc.Communicate(pc.points[16], pc.points[70]);
-            pc.Communicate(pc.points[17], pc.points[72]);
-            pc.Communicate(pc.points[72], pc.points[71]);
-            pc.Communicate(pc.points[18], pc.points[73]);
-            pc.Communicate(pc.points[19], pc.points[74]);
-            pc.Communicate(pc.points[20], pc.points[75]);
-            pc.Communicate(pc.points[21], pc.points[76]);
-            pc.Communicate(pc.points[22], pc.points[77]);
-            pc.Communicate(pc.points[24], pc.points[78]);
-        }
-        */
         public static void CreateTestTree(int count)
         {
             CreatePoints(count);
-            /*
-            pc.Communicate(pc.points[0], pc.points[1]);
-            pc.Communicate(pc.points[1], pc.points[2]);
-            pc.Communicate(pc.points[0], pc.points[9]);
-            pc.Communicate(pc.points[0], pc.points[8]);
-            pc.Communicate(pc.points[2], pc.points[5]);
-            pc.Communicate(pc.points[5], pc.points[6]);
-            pc.Communicate(pc.points[5], pc.points[3]);
-            pc.Communicate(pc.points[2], pc.points[3]);
-            pc.Communicate(pc.points[3], pc.points[4]);
-            pc.Communicate(pc.points[2], pc.points[7]);
-            */
+
             Communicate(points[0], points[1]);
             Communicate(points[1], points[4]);
             Communicate(points[4], points[2]);
             Communicate(points[4], points[3]);
             Communicate(points[4], points[5]);
         }
+        [STAThread]
         static void Main(string[] args)
         {
-
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            
+            /*
             CreateTestTree(6);
 
             RutWayBuilder builder = new RutWayBuilder();
@@ -138,6 +41,7 @@ namespace DirectionsAB
             }
 
             Console.ReadKey();
+            */
         }
     }
 }
