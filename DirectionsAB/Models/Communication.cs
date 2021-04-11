2e29c14d05919d9a54dc37f19c6ebec168dfdc54
DefautName <ilyakuzin20013107@gmail.com>
2021-04-11 12:36:12 +0300

    Added 2-4 levels and attributes in Communication

--- DirectionsAB/Models/Communication.cs
+++ DirectionsAB/Models/Communication.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace DirectionsAB.Models
 {
     class Communication
     {
+       public int RegionID { get; set; }
+       [Key]
+       public int CommID { get; set; }
     }
 }
