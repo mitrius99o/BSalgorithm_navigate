aa18fb00e9f3bd627bf8b2075263882a48d89530
DefautName <ilyakuzin20013107@gmail.com>
2021-04-11 22:49:53 +0300

    Added 2-4 levels and attibutes in Communication

--- DirectionsAB/Models/Communication.cs
+++ DirectionsAB/Models/Communication.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,9 @@ namespace DirectionsAB.Models
 {
     class Communication
     {
+
+        public int RegionID { get; set; }
+        [Key]
+        public int CommID { get; set; }
     }
 }
--- DirectionsAB/Models/Region.cs
+++ DirectionsAB/Models/Region.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace DirectionsAB.Models
 {
     public class Region
     {
+        [Key]
         public int RegionId { get; set; }
         public string Name { get; set; }
         public int X1 { get; set; }
